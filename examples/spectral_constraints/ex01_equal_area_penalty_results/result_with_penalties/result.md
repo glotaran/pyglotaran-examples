4faf0db6cff9a8adaa680fda79044530df4f1996
Joris Snellenburg <jsnel@users.noreply.github.com>
2020-09-28 23:45:06 +0200

    Added self-contained example demonstrating equal area penalties

---
+++ examples/spectral_constraints/ex01_equal_area_penalty_results/result_with_penalties/result.md
@@ -0,0 +1,65 @@
+Optimization Result            |            |
+-------------------------------|------------|
+ Number of residual evaluation |          6 |
+           Number of variables |          5 |
+          Number of datapoints |   (25552,) |
+            Degrees of freedom |      25547 |
+                    Chi Square |   6.18e+01 |
+            Reduced Chi Square |   2.42e-03 |
+        Root Mean Square Error |   4.92e-02 |
+
+
+# Model
+
+_Type_: kinetic-spectrum
+
+## Initial Concentration
+
+* **j1**:
+  * *Label*: j1
+  * *Compartments*: ['s1', 's2']
+  * *Parameters*: [i.1: **5.00000e-01** *(fixed)*, i.2: **6.81094e-01** *(StdErr: 3e-04 ,initial: 5.00000e-01)*]
+  * *Exclude From Normalize*: []
+
+## K Matrix
+
+* **k1**:
+  * *Label*: k1
+  * *Matrix*: 
+    * *('s1', 's1')*: kinetic.1: **2.50031e-01** *(StdErr: 5e-05 ,initial: 2.00000e-01)*
+    * *('s2', 's2')*: kinetic.2: **9.99947e-01** *(StdErr: 2e-04 ,initial: 1.10000e+00)*
+  
+
+## Irf
+
+* **irf1** (gaussian):
+  * *Label*: irf1
+  * *Type*: gaussian
+  * *Center*: irf.1: **4.00002e-01** *(StdErr: 6e-06 ,initial: 4.00000e-01)*
+  * *Width*: irf.2: **5.99785e-02** *(StdErr: 8e-06 ,initial: 5.00000e-02)*
+  * *Normalize*: False
+  * *Backsweep*: False
+
+## Dataset
+
+* **dataset1**:
+  * *Label*: dataset1
+  * *Megacomplex*: ['mc1']
+  * *Initial Concentration*: j1
+  * *Irf*: irf1
+
+## Megacomplex
+
+* **mc1**:
+  * *Label*: mc1
+  * *K Matrix*: ['k1']
+
+## Equal Area Penalties
+
+* 
+  * *Compartment*: s2
+  * *Interval*: [(0, 3000)]
+  * *Target*: s1
+  * *Parameter*: pen.1: **1.00000e+00** *(fixed)*
+  * *Weight*: 0.1
+
---
+++ examples/spectral_constraints/ex01_equal_area_penalty_script.py
@@ -0,0 +1,143 @@
+from copy import deepcopy
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
+
+from glotaran.builtin.models.kinetic_spectrum import KineticSpectrumModel
+from glotaran.parameter import ParameterGroup
+from glotaran.io import read_data_file
+
+script_dir = Path(__file__).resolve().parent
+data_path = script_dir.joinpath("ex01_equal_area_penalty_data.ascii")
+
+
+def plot_overview(res, title=None):
+    """ simple plotting function derived from code from pyglotaran_extras """
+    fig, ax = plt.subplots(2, 3, figsize=(12, 6), constrained_layout=True)
+    if title:
+        fig.suptitle(title, fontsize=16)
+    sas = res.species_associated_spectra
+    traces = res.species_concentration
+    if "spectral" in traces.coords:
+        traces.sel(spectral=res.spectral.values[0], method="nearest").plot.line(
+            x="time", ax=ax[0, 0]
+        )
+    else:
+        traces.plot.line(x="time", ax=ax[0, 0])
+    sas.plot.line(x="spectral", ax=ax[0, 1])
+    rLSV = res.residual_left_singular_vectors
+    rLSV.isel(left_singular_value_index=range(0, min(2, len(rLSV)))).plot.line(
+        x="time", ax=ax[1, 0]
+    )
+    ax[1, 0].set_title("res. LSV")
+    rRSV = res.residual_right_singular_vectors
+    rRSV.isel(right_singular_value_index=range(0, min(2, len(rRSV)))).plot.line(
+        x="spectral", ax=ax[1, 1]
+    )
+    ax[1, 1].set_title("res. RSV")
+    res.data.plot(x="time", ax=ax[0, 2])
+    ax[0, 2].set_title("data")
+    res.residual.plot(x="time", ax=ax[1, 2])
+    ax[1, 2].set_title("residual")
+    plt.show(block=False)
+    return fig
+
+
+def ex01_equal_area_penalty():
+
+    data = read_data_file(data_path)
+    result_folder = script_dir.joinpath("ex01_equal_area_penalty_results")
+
+    mspec_base = {
+        "initial_concentration": {
+            "j1": {
+                "compartments": ["s1", "s2"],
+                "parameters": ["i.1", "i.2"],
+                # "exclude_from_normalize": ["s1", "s2"],
+            },
+        },
+        "megacomplex": {
+            "mc1": {"k_matrix": ["k1"]},
+        },
+        "k_matrix": {
+            "k1": {
+                "matrix": {
+                    ("s1", "s1"): "kinetic.1",
+                    ("s2", "s2"): "kinetic.2",
+                }
+            }
+        },
+        "irf": {
+            "irf1": {"type": "gaussian", "center": "irf.1", "width": "irf.2"},
+        },
+        "dataset": {
+            "dataset1": {
+                "initial_concentration": "j1",
+                "megacomplex": ["mc1"],
+                "irf": "irf1",
+            },
+        },
+    }
+
+    mspec_equ_area = {
+        "equal_area_penalties": [
+            {
+                "compartment": "s2",
+                "target": "s1",
+                "parameter": "pen.1",
+                "interval": [(0, 3000)],
+                "weight": 0.1,
+            },
+        ],
+    }
+    mspec_np = deepcopy(mspec_base)
+    mspec_wp = dict(deepcopy(mspec_base), **mspec_equ_area)
+
+    model_without_penalty = KineticSpectrumModel.from_dict(mspec_np)
+    model_with_penalty = KineticSpectrumModel.from_dict(mspec_wp)
+
+    pspec_base = {
+        "kinetic": [0.2, 1.1],
+        "i": [0.5, 0.5, {"vary": False}],
+        "irf": [0.4, 0.05],
+        "pen": [1, {"vary": False}],
+    }
+    pspec_fit = {"i": [[0.5, {"vary": False}], [0.5, {"vary": True}]]}
+
+    pspec_np = dict(deepcopy(pspec_base))
+    del pspec_np["pen"]
+    pspec_wp = dict(deepcopy(pspec_base), **pspec_fit)
+
+    param_np = ParameterGroup.from_dict(deepcopy(pspec_np))
+    param_wp = ParameterGroup.from_dict(deepcopy(pspec_wp))
+
+    result_np = model_without_penalty.optimize(param_np, {"dataset1": data}, nnls=True)
+    folder_np = result_folder.joinpath("result_no_penalties")
+    fig_np = plot_overview(result_np.data["dataset1"], "without penalties")
+    fig_np.savefig(folder_np.joinpath(f"plot_overview_np.pdf"), bbox_inches="tight")
+    print(result_np.optimized_parameter)
+    result_np.save(str(folder_np))
+
+    result_wp = model_with_penalty.optimize(param_wp, {"dataset1": data}, nnls=True)
+    folder_wp = result_folder.joinpath("result_with_penalties")
+    fig_wp = plot_overview(result_wp.data["dataset1"], "with penalties")
+    fig_wp.savefig(folder_wp.joinpath(f"plot_overview_wp.pdf"), bbox_inches="tight")
+    print(result_wp.optimized_parameter)
+    result_wp.save(str(folder_wp))
+
+    res_wp = result_wp.data["dataset1"]
+    res_np = result_np.data["dataset1"]
+
+    area_s1_np = np.sum(res_np.species_associated_spectra.sel(species="s1"))
+    area_s2_np = np.sum(res_np.species_associated_spectra.sel(species="s2"))
+    area_s1_wp = np.sum(res_wp.species_associated_spectra.sel(species="s1"))
+    area_s2_wp = np.sum(res_wp.species_associated_spectra.sel(species="s2"))
+    print(f"  no penalties: area1: {area_s1_np}\narea2: {area_s2_np}\n")
+    print(f"with penalties: area1: {area_s1_wp}\narea2: {area_s2_wp}\n")
+    plt.show()
+
+
+if __name__ == "__main__":
+    ex01_equal_area_penalty()
